86feba44484cde90a30ebd019df5b89d4a36cde7
alsubaieali <alsubaie.ali.b@gmail.com>
2018-12-16 14:22:31 +0300

    Shops now can be added by admin users

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,12 +4,27 @@ class ShopsController < ApplicationController
   end
 
   def show
+    @shop = Shop.find_by(id: params[:id])
   end
 
   def new
     @shop = Shop.new
   end
 
+  def create
+    shop = Shop.create(shop_params)
+    redirect_to shops_path
+  end
+
   def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:name, :phone, :website, :twitter, :instagram)
   end
 end
