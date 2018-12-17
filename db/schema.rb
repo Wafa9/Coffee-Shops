1c4c96e8a2a9f272b70436e1cdba3297df43fbb2
Wafa9 <wafa.a.t14@gmail.com>
2018-12-17 10:47:39 +0300

    adding Commnets modle and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_16_081235) do
+ActiveRecord::Schema.define(version: 2018_12_17_074107) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
