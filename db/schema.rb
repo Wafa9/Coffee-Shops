b3a46b8b2ce65394fbe75711e4529419f26c2e03
Wafa9 <wafa.a.t14@gmail.com>
2018-12-18 12:11:47 +0300

    adding lat And Lon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_17_083100) do
+ActiveRecord::Schema.define(version: 2018_12_18_090702) do
 
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
