d8903845dae3f1df711058a9aa47829168598dd5
alsubaieali <alsubaie.ali.b@gmail.com>
2018-12-18 20:08:07 +0300

    Created the search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "/home", to: "pages#home", as: "home"
   get "/comments/show"
   get "/comments/edit"
+  get "/search", to: "pages#search"
   devise_for :users
   resources :shops
   resources :products
