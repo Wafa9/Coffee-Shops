e4d4f9605a137d658a6440eaec0668399e81a7c5
alsubaieali <alsubaie.ali.b@gmail.com>
2018-12-19 15:11:27 +0300

    Added Search functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,20 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def search
+    @term = params[:term]
+    shops = Shop.all
+    products = Product.all
+
+    @shops_match = []
+    @products_match = []
+
+    shops.each do |shop|
+      @shops_match.push(shop) if shop.name.downcase.include?(@term)
+    end
+    products.each do |product|
+      @products_match.push(product) if product.name.downcase.include?(@term)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get "/home", to: "pages#home", as: "home"
-  get "/comments/show"
-  get "/comments/edit"
   devise_for :users
   resources :shops
   resources :products
+  get "/home", to: "pages#home", as: "home"
+  get "/search", to: "pages#search"
+  get "/comments/show"
+  get "/comments/edit"
   root "pages#home"
 end
